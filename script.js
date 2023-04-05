18770ce615c7bafbe7a9a5c229c8ce1b9cd4aeeb
mhofmockel <matt.hofmockel@9thwonder.com>
2023-04-05 13:36:23 -0600

    Revert logic, add maxDepth global var

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const maxDepth = 5;
 const game_grid = Array.from({ length: 7 }, () => Array(6).fill(null));
 
 const player1 = {
@@ -13,7 +14,7 @@ const player2 = {
 const game = {
   board: game_grid,
   currentPlayer: player1,
-  place_chip: function (column) {
+  place_chip: function(column) {
     const rowIndex = this.board[column].indexOf(null);
     if (rowIndex === -1) return false;
     this.board[column][rowIndex] = this.currentPlayer;
@@ -97,8 +98,8 @@ const game = {
       boardElement.appendChild(columnElement);
     });
   },
-  reset_game: function () {
-    this.board = game_grid.map((col) => col.fill(null));
+  reset_game: function() {
+    this.board = game_grid.map(col => col.fill(null));
     this.paint_board();
 
     // Call AI player's move after resetting the game if the AI is the current player
@@ -108,7 +109,7 @@ const game = {
       }, 500);
     }
   },
-  analyze_board: function () {
+   analyze_board: function() {
     const find_winning_move = (player) => {
       for (let col = 0; col < this.board.length; col++) {
         const row = this.board[col].indexOf(null);
@@ -141,143 +142,54 @@ const game = {
     // Make a random move if no winning or blocking moves are found
     const availableColumns = this.board
       .map((col, index) => (col.includes(null) ? index : -1))
-      .filter((index) => index !== -1);
-    const randomMove =
-      availableColumns[Math.floor(Math.random() * availableColumns.length)];
-    console.log(
-      `AI found no winning or blocking moves and chose a random move in column ${randomMove}`
-    );
+      .filter(index => index !== -1);
+    const randomMove = availableColumns[Math.floor(Math.random() * availableColumns.length)];
+    console.log(`AI found no winning or blocking moves and chose a random move in column ${randomMove}`);
     return randomMove;
   },
 
-  is_missed_win: function (board, moveHistory, currentPlayer) {
-    if (moveHistory.length !== 3) return false;
-    if (
-      moveHistory[0].player !== currentPlayer ||
-      moveHistory[2].player !== currentPlayer
-    )
-      return false;
-    if (
-      moveHistory[0].column === moveHistory[2].column &&
-      moveHistory[1].player !== currentPlayer
-    ) {
-      return true;
-    }
-    return false;
-  },
+calculate_weights: function(board, currentPlayer, depth) {
+  if (depth === 0) return {};
 
-  calculate_weights: function (
-    board,
-    currentPlayer,
-    depth,
-    initialDepth,
-    moveHistory = []
-  ) {
-    if (depth === 0) return {};
+  const nextPlayer = currentPlayer === player1 ? player2 : player1;
+  const weights = {};
 
-    if (typeof initialDepth === "undefined") {
-      initialDepth = depth;
+  for (let col = 0; col < board.length; col++) {
+    const row = board[col].indexOf(null);
+    if (row === -1) continue;
+
+    board[col][row] = currentPlayer;
+
+    if (this.check_for_win(col, row)) {
+      weights[col] = currentPlayer.isAI ? 1 : -1;
+    } else {
+      const childWeights = this.calculate_weights(board, nextPlayer, depth - 1);
+      weights[col] = Object.values(childWeights).reduce((sum, weight) => sum + weight, 0);
     }
 
-    const nextPlayer = currentPlayer === player1 ? player2 : player1;
-    const weights = {};
-    const availableColumns = 7;
+    board[col][row] = null;
+  }
 
-    for (let col = 0; col < board.length; col++) {
-      const row = board[col].indexOf(null);
-      if (row === -1) continue;
+  // Only log the weights array when at the top level of depth (i.e., depth is equal to its initial value)
+  if (depth === maxDepth) {
+    const weightsTable = Object.keys(weights).map(col => ({
+      Depth: depth,
+      Column: col,
+      Weight: weights[col],
+    }));
+    console.table(weightsTable);
+  }
 
-      board[col][row] = currentPlayer;
-      moveHistory.push({ player: currentPlayer, column: col });
+  return weights;
+},
 
-      if (this.check_for_win(col, row)) {
-        const missedWin = this.is_missed_win(board, moveHistory, currentPlayer);
-        if (missedWin) {
-          console.log(
-            `Missed win occurred in column ${col}, treating it as a loss`
-          ); // Log message when missed win occurs
-        }
-        weights[col] = currentPlayer.isAI && !missedWin ? 1 : -1;
-        weights[col] *= Math.pow(availableColumns, initialDepth - depth);
-      } else {
-        const childWeights = this.calculate_weights(
-          board,
-          nextPlayer,
-          depth - 1,
-          initialDepth,
-          moveHistory.slice()
-        );
-        weights[col] = Object.values(childWeights).reduce(
-          (sum, weight) => sum + weight,
-          0
-        );
-      }
-
-      board[col][row] = null;
-      moveHistory.pop();
-    }
-
-    // Only log the weights array when at the top level of depth
-    if (depth === initialDepth) {
-      const weightsTable = Object.keys(weights).map((col) => ({
-        Depth: depth,
-        Column: col,
-        Weight: weights[col]
-      }));
-      console.table(weightsTable);
-    }
-
-    return weights;
-  },
-
-  make_ai_move: function () {
-    const depth = 4; // Define the desired depth
-    const weights = this.calculate_weights(
-      this.board,
-      this.currentPlayer,
-      depth
-    );
+  make_ai_move: function() {
+    const depth = maxDepth; // Define the desired depth
+    const weights = this.calculate_weights(this.board, this.currentPlayer, depth);
     const maxWeight = Math.max(...Object.values(weights));
-    let bestMove;
 
-    if (depth === 1) {
-      const nonZeroWeightMoves = Object.keys(weights).filter(
-        (col) => weights[col] !== 0
-      );
-      if (nonZeroWeightMoves.length > 0) {
-        bestMove = parseInt(
-          nonZeroWeightMoves[
-            Math.floor(Math.random() * nonZeroWeightMoves.length)
-          ]
-        );
-        console.log(
-          `AI evaluation (depth 1): Found non-zero weighted moves, randomly picking column ${bestMove}`
-        ); // Log message for depth 1 scenario
-      }
-    } else if (depth === 2) {
-      const nonNegativeWeights = Object.keys(weights).filter(
-        (col) => weights[col] >= 0
-      );
-      if (nonNegativeWeights.length === 1) {
-        bestMove = parseInt(nonNegativeWeights[0]);
-        console.log(
-          `AI evaluation (depth 2): Only one non-negative weighted column, playing column ${bestMove}`
-        ); // Log message for depth 2 scenario
-      }
-    }
-
-    if (typeof bestMove === "undefined") {
-      const bestMoves = Object.keys(weights).filter(
-        (col) => weights[col] === maxWeight
-      );
-      bestMove = parseInt(
-        bestMoves[Math.floor(Math.random() * bestMoves.length)]
-      );
-    }
-
-    console.log(
-      `AI chose column ${bestMove} with a score of ${weights[bestMove]}`
-    ); // Log the chosen move and its score
+    const bestMoves = Object.keys(weights).filter(col => weights[col] === maxWeight);
+    const bestMove = parseInt(bestMoves[Math.floor(Math.random() * bestMoves.length)]);
 
     this.place_chip(bestMove);
   },
